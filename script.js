fa060716b2eb7d67e8272bd66ba2052a94cff378
gabrielss97 <gabriel.ssalvador97@gmail.com>
2023-01-05 00:09:43 -0300

    autoplay

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function show(indexSum) {
   index = Math.min(Math.max(index, 0), liEls.length - 1);
   liEls[index].scrollIntoView({ behavior: "smooth" });
 }
+
+setInterval(() => {
+  if (index === liEls.length - 1) {
+    show(index - 2);
+  } else {
+    show(index + 1);
+  }
+}, 5000);
