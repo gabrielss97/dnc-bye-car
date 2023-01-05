f22d5b6c7e96db66fe1f5350dfec64b3d1f1ea86
Gabriel Soares Salvador <43815745+gabrielss97@users.noreply.github.com>
2023-01-05 18:40:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let liEls = document.querySelectorAll("ul li");
-let index = 0;
 
-function show(indexSum) {
-  index = index + indexSum;
-  index = Math.min(Math.max(index, 0), liEls.length - 1);
-  liEls[index].scrollIntoView({ behavior: "smooth" });
-}
-
-setInterval(() => {
-  if (index === liEls.length - 1) {
-    show(index - 2);
-  } else {
-    show(index + 1);
-  }
-}, 5000);
